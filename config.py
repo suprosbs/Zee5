a10a17d50f0854c9c2d49e17b623ae10309831d4
suprosbs <suproboiragiin@gmail.com>
2025-04-04 17:50:25 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,15 @@ import os
 class Config(object):
 
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6914028314:AAG09vTHsH2No_ock1Oqs4x61KJgFEUDJHg")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7619364034:AAHsuBrDWONcUzCPv8iMBehfh7GeojenSWk")
 
     # The Telegram API things
     # Get these values from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", 2944318))
-    API_HASH = os.environ.get("API_HASH", "e9746721493a910d201d898b4329da8f")
+    APP_ID = int(os.environ.get("APP_ID", 25194442))
+    API_HASH = os.environ.get("API_HASH", "9e93d41112872cc3bd58f4e29fd82c0a")
 
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "7651377821").split())
 
     # Banned Unwanted Members..
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
@@ -32,5 +32,5 @@ class Config(object):
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
     
     # Sql Database url
-    DB_URI = os.environ.get("DATABASE_URL", "")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://suproboiragi2:t4GwmmrWCkUcX3Ui@cluster0.nn4hh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
